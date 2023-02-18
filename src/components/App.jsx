ecfb9912f75c73b06cf3babca5ca59840312d109
Andrii Lysenko <s0fcatof@gmail.com>
2023-02-18 20:01:16 +0100

    Added contacts to be saved in local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,20 @@ export class App extends Component {
     super();
 
     this.state = {
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
+      contacts: [],
       filter: '',
     };
+  }
+
+  componentDidMount() {
+    this.setState({
+      ...this.state,
+      contacts: JSON.parse(localStorage.getItem('contacts')) || [],
+    });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
   }
 
   addContact = newContact => {
